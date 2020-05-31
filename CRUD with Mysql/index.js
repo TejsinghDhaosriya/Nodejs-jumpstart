8a20074c3cdd685d6324f60adf4d67cccb2a7233
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-05-31 21:33:01 +0530

    CRU operations

--- CRUD with Mysql/index.js
+++ CRUD with Mysql/index.js
@@ -1,3 +1,5 @@
+
+///midddleware
 var express = require('express');
 var app = express();
 var bodyParser  = require('body-parser');
@@ -10,3 +12,81 @@ app.use(cors());
 app.use(bodyParser.urlencoded({
     extended:true
 }));
+
+//connection
+var dbConn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'test-jumpstart'
+});
+//connect to database
+dbConn.connect();
+
+
+////-----------------------------///////////////
+app.get('/',function(req,res){
+    return res.send({error:true,message:'hellotej'})
+})
+//retrive all users 
+app.get('/information',function(req,res){
+    dbConn.query('Select * FROM info',function(error,results,fields){
+        if(error) throw error;
+        return res.send({error:false,data:results,message:'Complete Data'});
+    });
+});
+
+//Retrieve user with id
+app.get('/mydata/:Id',function(req,res){
+  console.log('0000000000000')
+    let id = req.params.Id;
+    
+    if(!id){
+        return res.status(400).send({error:true,message:'Please Provide Id'});
+    }
+    dbConn.query('select * from info where id=?',id,function(error,results,fields){
+        if(error) throw error;
+        return res.send({error:false,data:results[0],message:"Information by Id"});
+    });
+});
+//Add a new Record 
+app.post('/adduser',function(req,res){
+
+    let name = req.body.name;
+    let location = req.body.location;
+    console.log(name+"wwwwwwwwwww"+location);
+    if(!name && !location){
+        return res.status(4000).send({error:true,message:'Please Provide Information'});
+    }
+    dbConn.query("insert Into info(name,location) value(?,?)",[name,location],function(error,results,fields){
+   if(error) throw error;
+   return res.send({ error:false,data:results,message:"added recorded"});        
+    
+});
+});
+
+//upate a record
+app.put('/update',function(req,res){
+    console.log("dddddddddddddddddddddddddddd");
+    let id = req.body.id;
+    let name = req.body.name;
+    let location = req.body.location;
+    if(!id || !name || !location){
+        return res.status(400).send({error:user,message:'Please Provide Complete'});    
+    }
+    dbConn.query('update info set name=?,location=? where id=?',[name,location,id],function(error,results,fields){
+        if(error) throw error;
+        return res.send({error:false,data:results,message:'data updated'}); 
+    });
+});
+
+///listen
+
+
+app.listen(3000,function (){
+  console.log("node is running on 30000ww");
+} 
+    
+)
+
+module.exports=app;
