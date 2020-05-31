055cbb6b204e75f8ab0dcb2c05effd7db78f2e50
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-05-31 19:50:56 +0530

    CRUD

--- index.js
+++ index.js
@@ -15,5 +15,35 @@ router.get('/',function(req,res){
     res.json(jsondata);
 })  
 
+router.post('/postdata',function(req,res){
+   // console.log(req.body.Id);
+    if(req.body.Id && req.body.Title)
+     {
+       
+       jsondata.push(req.body);
+       res.json(jsondata);  
+     }
+     else{
+         console.log("put some value");
+     }
+})
+
+router.put('/updatedata/:Id',function(req,res){
+    console.log(req.params.Id)
+ if(req.params.Id){
+     _und.each(jsondata,function(ele,index)
+     {
+         if(req.params.Id==ele.Id){
+             ele.Title="Hello";
+             ele.Director="tez";
+         }
+     })
+   res.json(jsondata);
+    }
+
+    else{ console.log("Invalid Req");}
+
+})
+
 app.use('/api',router);
 app.listen(port);
