aaf8349c702439790336d71e6bd926861273840c
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-05-31 18:23:11 +0530

    Jumpstart

---
+++ index.js
@@ -0,0 +1,16 @@
+var express  = require('express');  
+var app =express();
+var bodyParser = require('body-parser');
+var jsondata = require('./movies.json');
+var _und = require('underscore');
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+router.get('/',function(req,res)){
+    res.json(jsondata);
+}
